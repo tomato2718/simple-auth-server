102ecece532bb0e2fad1d0a005051e2e915f06b6
tomato2718 <qweasdzxc030@gmail.com>
2025-05-19 11:19:26 +0800

    Feat: Create domain repository trait

--- src/domain/error.rs
+++ src/domain/error.rs
@@ -24,3 +24,25 @@ impl Display for ValidationError {
         write!(f, "{}", self.message)
     }
 }
+
+#[derive(Debug)]
+pub struct EntityConflict {}
+
+impl Error for EntityConflict {}
+
+impl Display for EntityConflict {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Entity already exist")
+    }
+}
+
+#[derive(Debug)]
+pub struct EntityNotExist {}
+
+impl Error for EntityNotExist {}
+
+impl Display for EntityNotExist {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Entity do not exist")
+    }
+}
