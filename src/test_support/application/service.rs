e9a350ac9ccbcaa409670c09bf222bb4414388ed
tomato2718 <qweasdzxc030@gmail.com>
2025-05-22 13:46:53 +0800

    Feat: Create FakeTokenIssuer

--- src/test_support/application/service.rs
+++ src/test_support/application/service.rs
@@ -1,9 +1,25 @@
-use crate::application::service::auth::PasswordHasher;
+use crate::application::service::auth::{PasswordHasher, TokenIssuer};
 
 pub struct FakePasswordHasher {}
 
 impl PasswordHasher for FakePasswordHasher {
     fn hash(&self, raw: &str) -> String {
         raw.to_string()
+    }
+}
+
+pub struct FakeTokenIssuer {
+    to_return: String
+}
+
+impl FakeTokenIssuer {
+    fn new(to_return: &str) -> Self {
+        FakeTokenIssuer { to_return: to_return.to_string() }
+    }
+}
+
+impl TokenIssuer for FakeTokenIssuer {
+    fn issue(&self, username: &str) -> String {
+        self.to_return.clone()
     }
 }
